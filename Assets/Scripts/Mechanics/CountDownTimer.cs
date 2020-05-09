31744a6157ed7d16529d6b275e90c8a3cd545249
Asrin Dayananda <adayanan@deakin.edu.au>
2020-05-09 13:50:00 +0100

    Count down timer script connected to timer, starts at 9 and goes negitive

---
+++ Assets/Scripts/Mechanics/CountDownTimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startTime = 10f;
+
+    [SerializeField] Text CountDownTimerText;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        CountDownTimerText.text = currentTime.ToString();
+    }
+}
